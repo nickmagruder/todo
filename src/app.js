ae08769d9e051e2fe59647902d9e3adb00769e14
nickmagruder <nick.magruder@outlook.com>
2021-03-30 22:17:46 -0700

    lab 32 in-progress

--- src/app.js
+++ src/app.js
@@ -1,11 +1,9 @@
 /* import { React, useState } from 'react'; */
 
 import ToDo from './components/todo/todo.js';
-
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
-
-/*   let [ list ] = useState(''); */
 
   return (
     <>
--- src/components/todo/list.js
+++ src/components/todo/list.js
@@ -1,8 +1,47 @@
 import React from 'react';
+import Toast from 'react-bootstrap/Toast';
+import Badge from 'react-bootstrap/Badge';
+/* import { propTypes } from 'react-bootstrap/esm/Image'; */
 
-function TodoList({list, handleComplete}){
+function TodoList({ list, handleComplete, handleDelete }) {
 
-    return (
+  const styles = {
+    pill: {
+      cursor: "pointer",
+    },
+  };
+
+
+  return (
+    <>
+      {list.map((item) => (
+        <Toast key={item._id} onClose={() => handleDelete(item._id)}>
+          <Toast.Header>
+            <Badge
+              pill
+              style={styles.pill}
+              variant={item.complete ? "danger" : "success"}
+              onClick={() => handleComplete(item._id)}
+            >
+              {!item.complete ? "Pending" : "Complete"}
+            </Badge>
+            <strong className="mr-auto">{item.assignee}</strong>
+          </Toast.Header>
+          <Toast.Body>
+            {item.text}
+      difficulty: {item.difficulty}
+          </Toast.Body>
+        </Toast>
+      ))}
+    </>
+  )
+}
+
+
+
+export default TodoList;
+
+{/*  return (
       <ul>
         {list.map(item => (
           <li
@@ -10,13 +49,14 @@ function TodoList({list, handleComplete}){
             key={item._id}
           >
             <span onClick={() => handleComplete(item._id)}>
-              {item.text}
+              <p>{item.assignee}</p>
+              <p>{item.text}</p>
+              <p>Difficulty: {item.difficulty}</p>
+              <button type="button">X</button>
+              <toast key=(item._id)
             </span>
           </li>
         ))}
       </ul>
     );
-  }
-
-
-export default TodoList;
+  }  */}
